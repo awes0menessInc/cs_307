f57b3959f034d24568e2ce1d17104f13325bff61
Bijin Chakraborty <14847418+BijinC@users.noreply.github.com>
2019-09-22 16:02:32 -0400

    Created testPost and postMicroblog functions for back-end

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,20 +1,12 @@
-// // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-// const functions = require('firebase-functions');
-//
-// // The Firebase Admin SDK to access the Firebase Realtime Database.
-// const admin = require('firebase-admin');
-// admin.initializeApp();
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-
-exports.test = functions.https.onRequest((req, res) => {
+exports.testGet = functions.https.onRequest((req, res) => {
     var contents = [];
     var db = admin.firestore();
     db.collection("test").get().then(snapshot => {
-
         snapshot.forEach(doc => {
             var v = {
                 "id": doc.id,
@@ -29,7 +21,75 @@ exports.test = functions.https.onRequest((req, res) => {
     })
 });
 
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.testPost = functions.https.onRequest((req, res) => {
+    var db = admin.firestore();
+    db.collection("test").add(req.body).then(snapshot => {
+        res.send('Added new document');
+        return "";
+    }).catch (exception => {
+        res.send(exception)
+    })
+});
+
+// As a logged-in user, when I view a userline/timeline, I should see posts
+// starting with the most recent posts at the top so that I can stay up-to-date
+// with the newest content.
+
+exports.getUserMicroblogs = functions.https.onRequest((req, res) => {
+    var contents = [];
+    var userId = req.params.id;
+    var db = admin.firestore();
+    db.collection("users").get(userId).then(snapshot => {
+        snapshot.forEach(doc => {
+            var object = {
+                "id": doc.id,
+                "content": doc.data().content,
+                "likes": doc.data().likes,
+                "quotes": doc.data().quotes,
+                "timestamp": doc.data().timestamp,
+                "topics": doc.data().topics,
+                "username": doc.data().username
+            }
+            contents = contents.concat(object);
+        });
+        res.send(contents)
+        return "";
+    }).catch(error => {
+        res.send(error)
+    })
+});
+
+// As a logged-in user, I should have the option to create a new microblog
+// directly from my timeline so that I can easily post to my timeline.
+
+exports.postMicroblog = functions.https.onRequest((req, res) => {
+
+    var db = admin.firestore();
+    // add to microblog collection
+    db.collection('microblogs').add(req.body).then(snapshot => {
+        res.send('Added new microblog successfully');
+        return "";
+      }).catch(error => {
+        res.send(error);
+    });
+
+    // // add ref to microblogs in user doc in user collection
+    // db.collection("users").get(userId).then(snapshot => {
+    //     snapshot.forEach(doc => {
+    //         var object = {
+    //             "id": doc.id,
+    //             "content": doc.data().content,
+    //             "likes": doc.data().likes,
+    //             "quotes": doc.data().quotes,
+    //             "timestamp": doc.data().timestamp,
+    //             "topics": doc.data().topics,
+    //             "username": doc.data().username
+    //         }
+    //         contents = contents.concat(object);
+    //     });
+    //     res.send(contents)
+    //     return "";
+    // }).catch(error => {
+    //     res.send(error)
+    // })
+});
