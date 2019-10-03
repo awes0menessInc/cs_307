ae7149a2424f507b41909ab2f09be8d184b346cd
Bijin Chakraborty <14847418+BijinC@users.noreply.github.com>
2019-10-03 13:33:11 -0400

    back-end functions

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -60,70 +60,31 @@ exports.getUserMicroblogs = functions.https.onRequest((req, res) => {
     })
 });
 
-// function get_user_microblogs(id) {
-//     var contents = [];
-//     var db = admin.firestore();
-//     db.collection("users").get(id).then(snapshot => {
-//         snapshot.forEach(doc => {
-//             var object = {
-//                 "id": doc.id,
-//                 "content": doc.data().content,
-//                 "likes": doc.data().likes,
-//                 "quotes": doc.data().quotes,
-//                 "timestamp": doc.data().timestamp,
-//                 "topics": doc.data().topics,
-//                 "username": doc.data().username
-//             }
-//             contents = contents.concat(object);
-//         });
-//         return contents;
-//     }).catch(error => {
-//         res.send(error)
-//     })
-// }
-
-// function get_chronological_order (microblogs) {
-
-// }
-
 // retrieve microblogs to populate a TIMELINE VIEW
-// exports.getAllMicroblogs = functions.https.onRequest((req, res) => {
-//     var db = admin.firestore();
-//     var ref = db.chil
-//     var contents = [];
+exports.getAllMicroblogs = functions.https.onRequest((req, res) => {
+    var db = admin.firestore();
+    var ref = db.ref('microblogs');
+    var contents = [];
     
+    db.collection('users').get(userId).then(doc => {
 
-    
-//     db.collection.('users').get(userId).then(doc => {
+        // query to get all microblogs of user whose topics exist in topics_following
+        // OR
+        // query microblogs collection where user exists in users_following and topic exists in topics_following
 
-//         // query to get all microblogs of user whose topics exist in topics_following
-//         // OR
-//         // query microblogs collection where user exists in users_following and topic exists in topics_following
+        var following = doc.data().following;
+        for (let i = 0; i < following.length; i++) {
+            let id = following[i];
+            contents.add(id);
+            // contents.add(get_user_microblogs(id));
+        }
+        res.send(contents);
+        return "";
 
-//         var following = doc.data().following;
-//         for (let i = 0; i < following.length; i++) {
-//             let id = following[i];
-            
-//             contents.add(get_user_microblogs(id));
-//         }
-
-//     }).catch(error => {
-//         res.send(error)
-//     })
-// });
-
-// db.collection("cities").doc("SF")
-//   .get()
-//   .then(function(doc) {
-//     if (doc.exists) {
-//       console.log("Document data:", doc.data());
-//     } else {
-//       // doc.data() will be undefined in this case
-//       console.log("No such document!");
-//     }
-//   }).catch(function(error) {
-//     console.log("Error getting document:", error);
-//   });
+    }).catch(error => {
+        res.send(error)
+    })
+});
 
 // As a logged-in user, I should have the option to create a new microblog
 // directly from my timeline so that I can easily post to my timeline.
